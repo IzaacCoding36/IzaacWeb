843eef0b16d4d8749086e711465fb183255f7197
Izaac <izaaccoding36@gmail.com>
2024-10-28 22:53:17 -0300

    IzaacWeb V3.6.03

--- js/botoes.js
+++ js/botoes.js
@@ -2,24 +2,27 @@
 
 document.querySelector('.filtro').addEventListener('click', () => {
     document.querySelector('html').style.setProperty('filter', 'none');
+    document.querySelector('html').style.setProperty('background-color', '#001403');
 });
 
 document.querySelector('.filtro2').addEventListener('click', () => {
     document.querySelector('html').style.setProperty('filter', 'sepia(2)');
+    document.querySelector('html').style.setProperty('background-color', '#3e3d26ff');
 });
 
 document.querySelector('.filtro3').addEventListener('click', () => {
     document.querySelector('html').style.setProperty('filter', 'grayscale(2)');
+    document.querySelector('html').style.setProperty('background-color', '#262626ff');
 });
 
 document.querySelector('.pt-br').addEventListener('click', () => {
-    alert('Você já está utilizando esta linguagem.');
+    alert("Você já está utilizando esta linguagem.");
 });
 
 document.querySelector('.maislang').addEventListener('click', () => {
-    alert('Para visualizar esta página em outras línguas, basta ativar a extensão do google tradutor em seu navegador.');
+    alert("Para visualizar esta página em outras línguas, basta ativar a extensão do google tradutor em seu navegador.");
 });
 
 document.querySelector('.principal').addEventListener('click', () => {
-    alert('Você já está na página principal.');
+    alert("Você já está na página principal.");
 });
