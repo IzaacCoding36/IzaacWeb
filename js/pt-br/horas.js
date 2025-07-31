a865b4af5d7436f3358caea3751a794a3cf13e19
Izaac <izaaccoding36@gmail.com>
2025-07-31 21:13:04 +0000

    IzaacWeb V4.1

--- js/pt-br/horas.js
+++ js/pt-br/horas.js
@@ -7,16 +7,6 @@ function displayTime(){
     var sec = dateTime.getSeconds();
     var session = document.getElementById('periodo');
 
-    if(hrs >= 12){
-        session.innerHTML = 'PM';
-    }else{
-        session.innerHTML = 'AM';
-    }
-
-    if(hrs > 12){
-        hrs = hrs - 12;
-    }
-
     document.getElementById('horas').innerHTML = hrs;
     document.getElementById('minutos').innerHTML = min;
     document.getElementById('segundos').innerHTML = sec;
